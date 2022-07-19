2a9b30e7b15fa9e9afe6db688b63f905a44dfa3e
苯苯 <91561865+robothot@users.noreply.github.com>
2022-07-19 17:55:34 +0800

    feat: 添加 Leaflet 项目作为地图打点，可以在web上预览 (#899)
    * Update TODO.md
    
    * feat: 添加 leaflet 作为地图预览
    
    Co-authored-by: jhonebee <zhangjin19960908@gamil.com>

---
+++ development/frontend/leaflet/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [['@babel/preset-env', {
+        targets: {
+            node: 'current',
+        }
+    }], '@babel/preset-react', '@babel/preset-typescript']
+};
